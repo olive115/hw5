ec8524f31368b0528ce16e4ff59540320850336b
olive115 <55379920+olive115@users.noreply.github.com>
2019-10-14 13:07:24 -0700

    Update horizontal-lines.js

--- horizontal-lines.js
+++ horizontal-lines.js
@@ -5,8 +5,7 @@ function setup() {
 function draw() {
   background(220);
   
-  // draw lots of vertical lines
-  // if i is less than width, increase i by 10
+  
   for(i = 0; i < height; i+=10){
     
     // i is equal to 0, 10, 20, 30, 40, 50, 60, 70, etc...
