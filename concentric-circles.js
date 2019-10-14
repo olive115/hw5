d14ba561a4c14e8594e9a66ed522038ce6dabc67
olive115 <55379920+olive115@users.noreply.github.com>
2019-10-14 14:26:57 -0700

    Update concentric-circles.js

--- concentric-circles.js
+++ concentric-circles.js
@@ -1,1 +1,21 @@
+function setup(){
+  createCanvas(400,400);
+ 
+  background(255);
 
+  
+  var x = 200;
+  var y = 200;
+  //number of circles
+  var circles = 40;
+  //for each circle
+  for (var total = 0; total < circles; total = total+1) {
+    //compute circle diameter based on reverse index (circles-total) (or max-current)
+    var diameter = (circles-total) * 10;
+   
+    //render the circle
+    ellipse(x,y, diameter, diameter);
+    
+  }
+  
+}
