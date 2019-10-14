8aee4171e8f3725fd08ae8c4d22d5d5c22471db3
olive115 <55379920+olive115@users.noreply.github.com>
2019-10-14 12:32:41 -0700

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,1 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+  
+
+  for (var i = 10; i < height/2; i += 10)
+    line(200 - i, i, 200 + i, i);
+}
+
+  for(var i = 10; i < 200; i += 10) {
+    line(i, i + 200, i, 400 - i, i + 200);
+    
+  }
+}
