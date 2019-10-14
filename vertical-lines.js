82a166341a812077f2f3920d65abaaea0b1cc134
olive115 <55379920+olive115@users.noreply.github.com>
2019-10-14 13:04:25 -0700

    Update vertical-lines.js

--- vertical-lines.js
+++ vertical-lines.js
@@ -1,1 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+  
+  // draw lots of vertical lines
+  // if i is less than width, increase i by 10
+  for(i = 0; i < height; i+=10){
+    
+    // i is equal to 0, 10, 20, 30, 40, 50, 60, 70, etc...
+    line(i, 0, i, i+ 1000);
+    
+  }
+}
